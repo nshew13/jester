6de0c047d3a0c0b1feca60418c718f1b587ed093
nshew13 <328432+nshew13@users.noreply.github.com>
2025-08-31 13:00:06 -0400

    adds Quasar

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@
 
 import {createApp} from 'vue';
 import {createPinia} from 'pinia';
+import { Quasar } from 'quasar';
 import App from './App.vue';
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 
 const pinia = createPinia();
 const app = createApp(App);
 
+app.use(Quasar, {});
 app.use(pinia);
 app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import {quasar, transformAssetUrls} from '@quasar/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
+	plugins: [
+		vue({
+			template: {transformAssetUrls},
+		}),
+		vueDevTools(),
+		// @quasar/plugin-vite options list:
+		// https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+		quasar({
+			autoImportComponentCase: 'kebab',
+			sassVariables: fileURLToPath(
+				new URL('./src/quasar-variables.sass', import.meta.url),
+			),
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+});
